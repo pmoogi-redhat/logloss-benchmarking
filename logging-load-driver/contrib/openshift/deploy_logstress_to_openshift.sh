a2638a72e61c74c692f80a32f6fb907b897100a3
Eran Raichstein <eran.raichstein@gmail.com>
2021-03-10 15:25:19 +0200

    first working version of stress with fluentd and calculate cpu/memory

--- logging-load-driver/contrib/openshift/deploy_logstress_to_openshift.sh
+++ logging-load-driver/contrib/openshift/deploy_logstress_to_openshift.sh
@@ -34,15 +34,15 @@ EOF
   fi
 }
 
-# delete logstress project if it exists (to get new frech deployment)
+# delete logstress project if it exists (to get new fresh deployment)
 delete_logstress_project_if_exists() {
 PROJECT_LOGSTRESS=$(oc get project | grep logstress)
 if [ ! -z "$PROJECT_LOGSTRESS" ]; then 
   echo "--> Deleting logstress namespace" 
   oc delete project logstress
   while : ; do
-    PROJECT_SKYDIVE=$(oc get project | grep logstress)
-    if [ -z "$PROJECT_SKYDIVE" ]; then break; fi 
+    PROJECT_LOGSTRESS=$(oc get project | grep logstress)
+    if [ -z "$PROJECT_LOGSTRESS" ]; then break; fi 
     sleep 1
   done
 fi
@@ -51,9 +51,15 @@ fi
 # create and switch context to logstress project
 create_logstress_project() {
   echo "--> Creating logstress namespace"
-  oc label nodes $NODE_TO_USE logstress=true
+  oc label nodes $NODE_TO_USE logstress=true --overwrite
   oc adm new-project --node-selector='logstress=true' logstress
   oc project logstress
+}
+
+# set credentials (allow privileged) 
+set_credentials() {
+  oc adm policy add-scc-to-user privileged -z default
+  oc adm policy add-cluster-role-to-user cluster-reader -z default
 }
 
 # deoploy logstress
@@ -64,19 +70,43 @@ deploy_logstress() {
   oc process -f $DEPLOY_YAML | oc apply -f -
 }
 
+# deoploy logcollector (fluentd)
+deploy_logcollector() {
+  DEPLOY_YAML=fluentd-template.yaml
+
+  echo "--> Deploying $DEPLOY_YAML"
+  oc delete configmap fluentd-config
+  oc create configmap fluentd-config --from-file=fluent.conf
+  oc process -f $DEPLOY_YAML | oc apply -f -
+}
+
+# deoploy capturestatistics
+deploy_capturestatistics() {
+  DEPLOY_YAML=capture-statistics-template.yaml
+
+  echo "--> Deploying $DEPLOY_YAML"
+  oc process -f $DEPLOY_YAML | oc apply -f -
+}
+
 evacuate_node_for_performance_tests() {
   echo "--> Evacuating $NODE_TO_USE"
   oc get pods --all-namespaces -o wide | grep $NODE_TO_USE
   
   oc adm cordon $NODE_TO_USE
-  oc adm drain $NODE_TO_USE --pod-selector='app!=low-log-stress' --pod-selector='app!=heavy-log-stress' --ignore-daemonsets=true --delete-local-data
+  oc adm drain $NODE_TO_USE --pod-selector='app notin (low-log-stress,heavy-log-stress,fluentd,capturestatistics)' --ignore-daemonsets=true --delete-local-data --force
 }
 
 return_node_to_normal() {
   echo "--> Allow sceduling on $NODE_TO_USE"
   oc adm uncordon $NODE_TO_USE
+  while : ; do
+    NODE_SCHEDULING_DISABLED=$(oc get nodes --selector='!node-role.kubernetes.io/master' | grep SchedulingDisabled)
+    if [ -z "$NODE_SCHEDULING_DISABLED" ]; then break; fi 
+    sleep 1
+  done
   oc get nodes --selector='!node-role.kubernetes.io/master'
 }
+
 
 # print pod status
 print_pods_status () {
@@ -86,7 +116,8 @@ print_pods_status () {
 
 # print usage insturctions
 print_usage_insturctions () {
-  echo -e "\n\n Expore logs on relevant pods \n\n"
+  echo -e "\n\nExpore logs of relevant pods\n"
+  echo -e "Expore logs of capturestatistics pod\n"
 }
 
 main() {
@@ -95,7 +126,10 @@ main() {
   return_node_to_normal
   delete_logstress_project_if_exists
   create_logstress_project
+  set_credentials
   deploy_logstress
+  deploy_logcollector
+  deploy_capturestatistics
   evacuate_node_for_performance_tests
   print_pods_status
   print_usage_insturctions
