6c16f35f40192381622a8f357b12df15d73ae98a
PRATIBHA MOOGI <pmoogi@redhat.com>
2021-05-20 19:18:05 +0530

    adding scripts to run log driver program

---
+++ file-rotate-test-basedon-periodicity-in-time.py
@@ -0,0 +1,40 @@
+import logging
+import logging.handlers
+import time
+import sys
+
+
+
+
+def payload_fixed(size):
+    return 'x' * size
+
+
+def main():
+
+    #one way to set upper bound to generate logs
+    MAXLOGLINES=1000000
+
+    interval=int(sys.argv[1])
+    rateofloglinesG=float(sys.argv[2])
+    logfilename=sys.argv[3]
+    backupFiles=int(sys.argv[4])
+
+    print("interval at which log rotation set",interval)
+    print("interval between two loglines",rateofloglinesG)
+
+    log = logging.getLogger(__name__)
+    fsize=1024
+    payloadfixedsize=payload_fixed(fsize)
+    trfh = logging.handlers.TimedRotatingFileHandler(logfilename,
+        encoding='utf-8', when='S', interval=interval, backupCount=backupFiles)
+    trfh.setFormatter(logging.Formatter('%(asctime)s %(message)s'))
+    log.addHandler(trfh)
+    count=0
+    while count < MAXLOGLINES :
+        time.sleep(rateofloglinesG)
+        count=count+1
+        log.warning(payloadfixedsize + ':' + str(count))
+    return
+
+main()
